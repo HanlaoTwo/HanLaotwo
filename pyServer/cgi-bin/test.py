84fe4748307c7bdacd624b8eea5e723d7b03ad72
HanlaoTwo <hankeboom@163.com>
2018-01-11 19:31:24 +0800

    修改编码，暂时使用GBK，前端可以正常显示

--- pyServer/cgi-bin/test.py
+++ pyServer/cgi-bin/test.py
@@ -1,3 +1,7 @@
-print('Access-Control-Allow-Origin: http://localhost:63342' + '\n')
+# -*- coding: utf-8 -*-
 
-print('朱丽霞是傻逼')
+print('Content-Type: text/html;charset=utf-8')
+print('Access-Control-Allow-Origin: *' + '\n')
+
+a = u'监听程序无法找到匹配协议栈的可用句柄.'
+print(a)

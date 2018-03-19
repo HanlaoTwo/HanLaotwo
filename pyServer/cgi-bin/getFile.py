434f9ca8e186de94da8da8c62f3c7a3a26d028a4
HanlaoTwo <hankeboom@163.com>
2018-03-19 20:09:46 +0800

    MOD

--- pyServer/cgi-bin/getFile.py
+++ pyServer/cgi-bin/getFile.py
@@ -4,7 +4,7 @@ f = open('D:/workspace/idea/HanLaotwo/Pages/content/haha',encoding='UTF-8')
 clog = f.read()
 
 #print('Accept: application/json, text/javascript, */*; q=0.01')
-print('Content-Type: text/html;charset=GBK')
+print('Content-Type: text/html;charset=utf-8')
 #print('Access-Control-Allow-Credentials: true')
 #print('Access-Control-Allow-Headers: accessToken,Content-Type')
 print('Access-Control-Allow-Origin: *' + '\n')

95c2775b9fdda7ba82f52656f9e853cd7734c654
hanlaowo <1451291941@qq.com>
2018-05-08 23:00:28 +0800

    null

---
+++ Pages/htmls/public/header.js
@@ -0,0 +1,19 @@
+/**
+ * Created by hq on 2018/5/8.
+ * .
+ */
+Vue.component('header_com', {
+    template: '#head_template',
+    //replace: true,
+    props: ['ci'],
+    data: function () {
+        return {
+            host_stata: [
+                {title: 'sdasasasdasda', date: "2017.01.01"},
+                {title: 'hs0xzczxczx1', date: "2017.01.01"},
+                {title: 'hszxczxvcvnbv01', date: "2017.01.01"},
+                {title: 'hs2342342342301', date: "2017.01.01"},
+                {title: 'hghjgfhjghjgs01', date: "2017.01.01"}]
+        }
+    }
+});

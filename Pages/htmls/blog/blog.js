01d820a73504435344853ae7c1f179aff28a005d
HanlaoTwo <hankeboom@163.com>
2018-05-24 16:37:37 +0800

    合并组件，简化逻辑。 更新url

--- Pages/htmls/blog/blog.js
+++ Pages/htmls/blog/blog.js
@@ -3,31 +3,52 @@
  * ,.
  */
 var baseUrl = "http://127.0.0.1:5000";
+var getArticle = "/get_article";
+var getContent = "/get_conetnt";
 var demo = new Vue({
     el: '#blog',
     created: function () {
-        //this.getClusters()
+        this.getCatalog();
+        this.getContent("","")
     },
     data: {
-        clusters:["1","2"],
-        clusters0: [],
-        gridColumns: ['host', 'state'],
-        gridData: []
+        catalog:[{'name': 'blog', 'files': ['blog.css', 'blog.html']}, {'name': 'index', 'files': ['index.css', 'index.html']}],
+        article: "### shen me dong xi",
+        content: [],
     },
     methods: {
-        getClusters: function () {
+        getCatalog: function () {
             ins = this;
             return $.ajax({
                 type: 'get',
-                url: baseUrl + '/getComps',
+                url: baseUrl + getContent,
                 data: {},
                 success: function (data) {
                     console.log(data);
-                    ins.clusters = setClusters(data);
-                    console.log(ins.clusters)
+                    ins.catalog = JSON.parse(data);
+                    console.log(ins.catalog)
                 },
                 error: function () {
                     //do something
+                }
+            });
+        },
+        getContent: function (dir,name) {
+            ins = this;
+            return $.ajax({
+                type: 'get',
+                url: baseUrl + getArticle,
+                data: {
+                    dir:dir==""?"blog":dir,
+                    name:name==""?"blog.js":name
+                },
+                success: function (data) {
+                    console.log(data);
+                    ins.article =  marked(data);
+                    console.log(ins.article)
+                },
+                error: function () {
+                    console.log(dir+"  "+name);
                 }
             });
         }
